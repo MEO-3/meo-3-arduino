cd145ab16626337467974edba33ed65f64770ec3
lgthvinh <everwellmax@gmail.com>
2026-01-09 18:24:14 +0700

    Remove commented-out setup and loop functions from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -59,3 +59,14 @@ void loop() {
         meoLogger("INFO", success ? "Published humid_temp_update event" : "Failed to publish event");
     }
 }
+
+// void setup() {
+//     // put your setup code here, to run once:
+//     Serial.begin(115200);
+// }
+
+// void loop() {
+//     // put your main code here, to run repeatedly:
+//     Serial.println("Hello, World!");
+//     delay(1000);
+// }
